3cfbc8c6f2f730ecd06c7163d802ad8218131b6d
Kerry Street <kerrycstreet@gmail.com>
2013-08-21 14:26:40 -0700

    Add boilerplate PluginLoader class

---
+++ PortableMvxAsync.MvxPlugins.SimpleAsync/PluginLoader.cs
@@ -0,0 +1,22 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.CrossCore.Plugins;
+
+namespace PortableMvxAsync.MvxPlugins.SimpleAsync
+{
+    // all Mvx PLugins have this same boilerplate code using this naming
+    public class PluginLoader : IMvxPluginLoader
+    {
+
+        // another part of the Plugin convention is to add an instance of self
+        public static readonly PluginLoader Instance = new PluginLoader();
+
+        public void EnsureLoaded()
+        {
+            var pluginMgr = Mvx.Resolve<IMvxPluginManager>();
+
+            // find the associated Plugin file for my platform based on my namespace name
+            pluginMgr.EnsurePlatformAdaptionLoaded<PluginLoader>();
+        }
+    }
+}
+
