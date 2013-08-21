6a607e973bd2939cb19fefb00262e64d47f80bf0
Kerry Street <kerrycstreet@gmail.com>
2013-08-21 14:46:32 -0700

    Add WP Implementation and Plugin setup

---
+++ PortableMvxAsync.MvxPlugins.SimpleAsync.WindowsPhone/MySimpleAsyncTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace PortableMvxAsync.MvxPlugins.SimpleAsync.WindowsPhone
+{
+    public class MySimpleAsyncTest : ISimpleAsyncTester
+    {
+        // note that this method does NOT need "async" because it is not awaiting anything
+        // it is doing the work that the CALLER will be "await" to be done
+        public Task<string> LongRunningMethodAsync(string message)
+        {
+            // use this old school one for now
+            return Task.Factory.StartNew<string>(() => LongRunningMethod(message));
+
+            // Note that I cannot use original Task.Run here with references I have..
+            // return Task.Run<string>(() => LongRunningMethod(message));
+
+            // TODO: I don't have access to Task.Run BUT I do have TaskEx.Run in Async Lib?.  Is that okay to use?
+            // return TaskEx.Run<string>(() => LongRunningMethod(message));
+
+        }
+
+        private string LongRunningMethod(string message)
+        {
+            Thread.Sleep(2000);
+            return "Hello " + message;
+        }
+    }
+}
+
---
+++ PortableMvxAsync.MvxPlugins.SimpleAsync.WindowsPhone/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.CrossCore.Plugins;
+
+namespace PortableMvxAsync.MvxPlugins.SimpleAsync.WindowsPhone
+{
+    // Plugin boilerplate code for the Implementation-side of an Mvx Plugin
+    public class Plugin : IMvxPlugin
+    {
+        // you can be certain that this
+        // Plugin Load method will only be called once
+        public void Load()
+        {
+            // can use the same IoC methods here that
+            // you use in Setup and App
+
+            // whenever someone asks for an ISimpleAsyncTester on THIS platform
+            // give them a MySimpleAsyncTest from this DLL
+            Mvx.RegisterType<ISimpleAsyncTester, MySimpleAsyncTest>();
+        }
+    }
+}
